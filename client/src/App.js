b0617f84833857facaecf3d4983c901d29702830
Marina Baskova <marinabaskova24@gmail.com>
2019-12-16 12:57:15 -0500

    landing summary

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Navigation from './components/Navigation';
-import Summary from './components/Summary';
-import Skils from './components/Skills'
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import React from "react";
+import Navigation from "./components/Navigation";
+import Summary from "./components/Summary";
+import Skils from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
-import './index.css';
+import "./index.css";
 
 function App() {
-	return (
+  return (
     <>
-    <Navigation />
-		<div className="App">
-			<Summary />
-      <Skils/>
-			<Projects />
-			<Contact />
-		</div>
+      <Navigation />
+      <div className="App">
+        <Summary />
+        <Skils />
+        <Projects />
+        <Contact />
+      </div>
     </>
-	);
+  );
 }
 
 export default App;
