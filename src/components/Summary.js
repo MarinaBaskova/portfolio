ddc1e0b2a25e82760902559223dc8bfc02743d1b
Marina Baskova <marinabaskova24@gmail.com>
2019-12-16 19:40:35 -0500

    changed summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -10,11 +10,11 @@ const Summary = () => {
       <div className="summary-text">
         <p className="summary">
           Hi, my name is Marina. I'm a full-stack web engineer with experience
-          in leading software development and makeup artistry. My prior
-          experience helped me to develop collaborating, problem-solving, and
-          organizational skills. I'm excited to bring those skills into my web
-          development journey. If I'm not programming, I'm likely hiking or
-          observing the night sky with my telescope, thinking about new
+          in software development, project management and makeup artistry. My
+          prior experience helped me to develop collaboration, problem-solving,
+          and organizational skills. I'm excited to bring those skills into my
+          web development journey. When I'm not programming, I'm likely hiking
+          or observing the night sky with my telescope, thinking about new
           technologies, or trying an original recipe.
         </p>
       </div>
